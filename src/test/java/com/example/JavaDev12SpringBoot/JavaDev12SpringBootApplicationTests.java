1807282d7d9bbd67f68c59ac382f0d9fab815c48
radcheenkoo <134805103+radcheenkoo@users.noreply.github.com>
2024-01-14 17:41:47 +0200

    third commit

---
+++ src/test/java/com/example/JavaDev12SpringBoot/JavaDev12SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaDev12SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDev12SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
